4039e6953912a7322d1ef12acad828360b3b8d5d
Dragon balance <124173984+Marcos7485@users.noreply.github.com>
2023-04-14 12:18:03 -0300

    Delete core/views directory

--- core/views/admin/Layouts/html_footer.php
+++
@@ -1,7 +0,0 @@
-
-<script src="https://llicorellapriorato.store/public/assets/js/bootstrap.bundle.min.js"></script>
-<script src="https://llicorellapriorato.store/public/assets/js/axios.min.js"></script>
-<script src="https://llicorellapriorato.store/public/assets/js/app.js"></script>
-<script src="https://llicorellapriorato.store/public/assets/js/toastr.min.js"></script>
-</body>
-</html>
--- core/views/vendedor/Layouts/html_footer.php
+++
@@ -1,7 +0,0 @@
-
-<script src="../assets/js/bootstrap.bundle.min.js"></script>
-<script src="../assets/js/axios.min.js"></script>
-<script src="../assets/js/app.js"></script>
-<script src="../assets/js/toastr.min.js"></script>
-</body>
-</html>
